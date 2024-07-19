770b720b33d04163cf0384ac283a26e9e2c80c02
haruki7049 <tontonkirikiri@gmail.com>
2024-07-19 12:55:31 +0900

    chore(NixFile): Init nix files

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    zig.url = "github:mitchellh/zig-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+  };
+
+  outputs = { self, nixpkgs, systems, flake-utils, zig, treefmt-nix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays =
+          [ (final: prev: { zigpkgs = zig.packages.${prev.system}; }) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
+      in
+      {
+        # Use `nix fmt`
+        formatter = treefmtEval.config.build.wrapper;
+
+        # Use `nix flake check`
+        checks.formatting = treefmtEval.config.build.check self;
+
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [ zigpkgs."0.13.0" ];
+
+          shellHook = ''
+            export PS1="\n[nix-shell:\w]$ "
+          '';
+        };
+      });
+}
