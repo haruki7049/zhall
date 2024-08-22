62ebabd8e5ee2fba94971aafa91e16982a966dcd
haruki7049 <tontonkirikiri@gmail.com>
2024-08-22 15:05:53 +0900

    build(flake.nix): Init packages.default

--- flake.nix
+++ flake.nix
@@ -8,15 +8,40 @@
   outputs = { self, nixpkgs, flake-utils, treefmt-nix }:
     flake-utils.lib.eachDefaultSystem (system:
       let
+        pname = "zhall";
         pkgs = import nixpkgs { inherit system; };
         treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
+        zhall = pkgs.stdenv.mkDerivation {
+          inherit pname;
+          version = "0.1.0";
+          src = ./.;
+
+          nativeBuildInputs = [
+            pkgs.zig_0_13.hook
+          ];
+        };
       in
       {
         # Use `nix fmt`
         formatter = treefmtEval.config.build.wrapper;
 
         # Use `nix flake check`
-        checks.formatting = treefmtEval.config.build.check self;
+        checks = {
+          inherit zhall;
+          formatting = treefmtEval.config.build.check self;
+        };
+
+        # nix build .
+        # nix build .#default
+        packages = {
+          inherit zhall;
+          default = zhall;
+        };
+
+        # nix run .#default
+        apps.default = flake-utils.lib.mkApp {
+          drv = zhall;
+        };
 
         devShells.default = pkgs.mkShell {
           nativeBuildInputs = with pkgs; [
